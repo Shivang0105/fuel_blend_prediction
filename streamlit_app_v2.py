f0ce5367cc786f765c9811138a87041c4fb3ca3d
Shivang0105 <157299434+Shivang0105@users.noreply.github.com>
2025-08-16 07:35:03 +0000

    Launch Prediction Button

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -1071,7 +1071,7 @@ def main():
                 <p style="color:#005A9C;">Step inside the AI-powered lab that helps design sustainable fuel blends at scale.</p>
             </div>
             """, unsafe_allow_html=True)
-            col1, col2,col3 = st.columns([1.5,1,1.5])
+            col1, col2,col3 = st.columns([1,1,1])
             with col2:
                 if st.button("Launch Prediction",use_container_width=True):
                     st.session_state.step = 1
